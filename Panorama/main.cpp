a90f9a4bfc6ecfaf49dfdecf56fd92c3c8c32f71
Leo <leo.vornberger@gmx.de>
2021-01-30 22:19:10 +0100

    Initial Commit

---
+++ Panorama/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Panorama
+//
+//  Created by Leo Vornberger on 30.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
